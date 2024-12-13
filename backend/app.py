bc090c243ad4d65e7186b3903043a5df6553965c
saravananbs <bssaravanan247@gmail.com>
2024-12-13 21:56:08 +0530

    translated successfully need to add multiple language and loading icon

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template, send_file
 import os
 from Models.audio_translator import CustomTranslator
-
+#Hello
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 PROCESSED_FOLDER = 'processed'
